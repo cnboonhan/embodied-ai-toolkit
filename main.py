925d8aafbb4ddb724be77481b641defcb1da7836
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2025-08-24 11:59:14 +0800

    add label

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main(config_path: Path, launch_rerun: bool = True):
     if launch_rerun:
         rr.serve_grpc(grpc_port=config.data_grpc_port)
 
-    viser_server = viser.ViserServer(port=config.urdf_viewer_port, verbose=False)
+    viser_server = viser.ViserServer(port=config.urdf_viewer_port, verbose=False, label=config.label)
     urdf_robot = ViserUrdf(viser_server, load_urdf(config.urdf_path), load_meshes=True, load_collision_meshes=False)
     slider_handles, slider_names, custom_slider_handles, custom_slider_names = setup_ui(viser_server, urdf_robot, config)
 
